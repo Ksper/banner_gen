5ffde13c91aceb1f2092474589c25f31e45423e7
grich91300@gmail.com <Ridespiri1>
2022-01-18 01:39:36 +0100

    first commit

---
+++ src/components/MonButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const MonButton = () => {
+    return (
+        <Button>TOTO</Button>
+    )
+};
+
+const Button = styled.button`
+  background-color: #343434;
+  color: white;
+`;
